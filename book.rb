651a219197d7d7fe25a66cc7088be2fbc3960d6a
Carlos Barrera <barrera9696@gmail.com>
2019-05-20 10:41:02 -0500

    carlos

--- book.rb
+++ book.rb
@@ -1,1 +1,23 @@
+class Book 
+@@all = []
+    attr_accessor :author, :title, :word_count 
+    def initialize(author, title, word_count)
+        @author = author 
+        @title = title
+        @word_count = word_count
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end
+    
+    def author 
+        self.select do |book|
+        book == self 
+       end 
+    end
+
+
+end 
 
--- console.rb
+++ console.rb
@@ -3,4 +3,15 @@ require_relative './book'
 require_relative './author'
 require_relative './book_author.rb'
 
+
+carlos = Author.new("Carlos")
+sam = Author.new("Sam")
+
+ruby = Book.new(carlos, "Ruby", 100)
+java = Book.new(sam, "Java", 50)
+
+author_book1 = BookAuthor.new(carlos, ruby)
+author_book2 = BookAuthor.new(sam, java)
+
+
 binding.pry
