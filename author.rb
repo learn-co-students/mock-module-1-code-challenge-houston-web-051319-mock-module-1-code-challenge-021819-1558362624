411be204f812e6f4968c85c9dc5615cd43ba7192
Jason Fournier <jmf4nier@hotmail.com>
2019-05-20 13:04:49 -0500

    finished

--- author.rb
+++ author.rb
@@ -18,11 +18,29 @@ class Author
     book_four = Book.new(title, word_count)              
     BookAuthor.new(book_four, self)              
   end
+  def total_words
+    total = 0
+    BookAuthor.all.each do |obj|
+      if obj.author_obj.author_name == self.author_name
+        total += obj.book_obj.word_count
+      end
+    end
+    total
+  end
+  
   def self.most_words
-    
-    BookAuthor.all.map do |obj|
-      obj
-      
+    most = 0
+    author = []
+    Author.all.each do |author_obj|  
+      if author_obj.total_words > most 
+        most = author_obj.total_words
+        author = author_obj
+      end
     end
   end
+  #takes and array of all artists and iterates while calling total_words instance method on each 
+  #author object.  compare number to placeholder num and each other. return object with highest word_total.
 end
+
+
+
--- console.rb
+++ console.rb
@@ -4,14 +4,15 @@ require_relative './author'
 require_relative './book_author.rb'
 
 book_one = Book.new("War and Peace", 10000)
-book_two = Book.new("Jurassic Park", 9000)
+book_two = Book.new("Jurassic Park", 90000)
 book_three = Book.new("Jungle Book", 8000)
 author_one = Author.new("Rudyard Kipling")
 author_two = Author.new("Michael Crighton")
 author_three = Author.new("Tolstoy")
-bookauthor_one = BookAuthor.new(book_two, author_two)
-bookauthor_two = BookAuthor.new(book_one, author_three)
-bookauthor_three = BookAuthor.new(book_three, author_one)
+bookauthor_one = BookAuthor.new(book_two, author_one)
+bookauthor_two = BookAuthor.new(book_one, author_one)
+bookauthor_three = BookAuthor.new(book_three, author_two)
+
 
 p Author.most_words
 #binding.pry
