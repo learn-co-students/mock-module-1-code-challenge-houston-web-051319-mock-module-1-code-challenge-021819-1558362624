2c3b209b7f2e17b73fef10945fcff38b24e2300d
Stefan Scepanovic <scepa91@icloud.com>
2019-05-20 10:42:25 -0500

    -

--- author.rb
+++ author.rb
@@ -1,1 +1,35 @@
+require 'pry'
+class Author
+    @@all = []
+attr_accessor :book
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+a_author_books = Books.all.select |author_books|
+author_books = self
+end
+a_author_books.map do | author_books_arr |
+    author_books_arr
+end
+
+ 
+    def total_words 
+    author_books = Book.all.select | author |
+    author_books = self
+ end author_books
+end
+
+
+    def self.all
+        @@all
+    end
+
+end
+
+
+binding.pry
+
+
+
 
