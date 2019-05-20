e688ac2a91cfcbac7d4b0b1c220ceb637a53f957
feven tsegay <feventsegay6@gmail.com>
2019-05-20 10:41:13 -0500

    -

--- book.rb
+++ book.rb
@@ -1,1 +1,29 @@
+class Book 
+    attr_reader :bookTitle
+    @@all=[]
+    def initialize(bookName)
+        @bookName=bookName
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def author
+        Autor.all.select do | autorO|
+            autorO.book
+        end
+    end
+    
+    def title
+     Book.all.select do |title|
+        title.bookTitle
+     end
+    end
+
+    
+
+
+     
+end
