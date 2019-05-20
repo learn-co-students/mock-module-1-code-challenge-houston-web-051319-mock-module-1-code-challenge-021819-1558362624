9aa532b0c823e8928075080b5a4bd582934f9ce5
Maruf <marufbinawal@gmail.com>
2019-05-20 10:43:11 -0500

    mock challenge

--- book.rb
+++ book.rb
@@ -1,1 +1,17 @@
+class Book
+    attr_accessor :author, :title, :word_count
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+    
+    def initialize(author, title, word_count)
+        @author = author
+        @title = title
+        @word_count = word_count
+        @@all << self
+ 
+    end
+end
